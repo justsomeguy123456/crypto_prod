fd9b195c490669dd2b7bedf71095c258112d8c5b
Nick <nickabbasi1@gmail.com>
2021-07-19 20:18:41 -0400

    added xrp to portfolio.sh

--- portfolio.sh
+++ portfolio.sh
@@ -7,5 +7,6 @@ cd /home/nick/python_projects/crypto/crypto
 /home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/bnb_bal.py
 /home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/eth_bal.py
 /home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/btc_bal.py
+/home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/xrp_beb_bal.py
 /home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/get_coin_prices.py
 /home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/average_cost.py

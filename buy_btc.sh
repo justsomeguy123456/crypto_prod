108bce4af5c501188f6c96e1fc53896757f888d8
Nick <nickabbasi1@gmail.com>
2021-11-20 07:00:34 -0500

    changed buy btc script

--- buy_btc.sh
+++ buy_btc.sh
@@ -2,4 +2,4 @@
 cd /home/nick/python_projects/crypto/crypto_prod
 
 
-/home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/buy_coins.py BTC coinbasepro 25
+/home/nick/anaconda3/bin/python /home/nick/python_projects/crypto/crypto_prod/buy_coins.py BTC coinbasepro 15
